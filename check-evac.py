ef9d2f2813c1f54586d0312dc7e85200f062691d
anna x <anna.xing.22@gmail.com>
2020-02-15 07:03:19 -0500

    in progress dont use

--- check-evac.py
+++ check-evac.py
@@ -1,4 +1,4 @@
-import requests, json, os
+import requests, json, os, pandas, numpy, hurr_data_reader
 from flask import Flask, render_template, jsonify
 from flask_pymongo import PyMongo
 
@@ -7,6 +7,14 @@ template_path = os.path.join(project_root, './')
 
 app = Flask(__name__, template_folder=template_path)
 
+FIND_USER_ENDPOINT = ""
+API_KEY = "AIzaSyANV5fMhen4qk84sPxZyuQ4QypR3t1pz0I"
+
 @app.route('/')
 def render():
     return render_template('index.html')
+
+@app.route('/check_evac', methods=['GET', 'POST'])
+def check_evac(user_location):
+    for hurr_point in range(len(df1.index)):
+        
--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-$(function() {
+$(function () {
     $("a#process_input").bind("click")
 })
+
+function passUserLocation(x) {
+    $.getJSON($SCRIPT_ROOT + '/check_evac', {
+            post: x
+        }, function (data) {
+            let response = data.result;
+            console.log(response);
+        }
+    });
+}
